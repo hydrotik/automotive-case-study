41738cbeb884cc6bc034d4736abd3bfaab8a4839
Donovan adams <business@hydrotik.com>
2020-05-31 15:14:29 -0400

    move lick styles into project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import '../css/index.scss';
 import React from 'react';
 // import { render } from 'preact';
