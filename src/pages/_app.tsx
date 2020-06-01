a48ab660e3b98480c1c13bcff7b8dbb03051c977
Donovan adams <business@hydrotik.com>
2020-05-31 20:18:43 -0400

    copy carousel css tout of npm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../css/index.scss';
-import 'pure-react-carousel/dist/react-carousel.es.css';
 import React from 'react';
 // import { render } from 'preact';
 // import { PureComponent } from 'preact/compat';
